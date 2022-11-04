b467059d2b9cf227eab3b592da0f0cf5b7109a07
gamed <gamed@LAPTOP-H7SONEPA>
2022-11-04 00:22:14 -0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age; //Declare a variable
+            age = 15; //assigning a vaule 
+            int age2 = 17; // assing var 2 with info
+            int total =  age + age2; // two variables
+            Console.WriteLine("age: " + age + " + Age2: " + age2 + " is" + total); //print the variable
+            Console.ReadLine(); //keep it open
+        }
+    }
+}
