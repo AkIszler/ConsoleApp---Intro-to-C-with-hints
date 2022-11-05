4c9d876b97981745117ab6d6d40bce3f9ba04191
SoNotTheHeroType <114718803+AkIszler@users.noreply.github.com>
2022-11-05 00:49:07 -0700

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel;
 
 namespace ConsoleApp3
@@ -7,12 +7,33 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            int age; //Declare a variable
-            age = 15; //assigning a vaule 
-            int age2 = 17; // assing var 2 with info
-            int total =  age + age2; // two variables
-            Console.WriteLine("age: " + age + " + Age2: " + age2 + " is" + total); //print the variable
-            Console.ReadLine(); //keep it open
+           // int age; //Declare a variable
+           // age = 15; //assigning a vaule 
+           // int age2 = 17; // assing var 2 with info
+           // int total = age + age2; // two variables
+
+            // Console.WriteLine(age); //print the variable
+
+            // Console.WriteLine("age: " + age + " + Age2: " + age2 + " is" + total); //prints the ALL of the variable  
+            // Console.ReadLine(); //keep it open - Keep at end unless waiting for response.
+
+            // My demo code
+
+                    int bY;
+                    int cY;
+
+                    bY = 1991;
+                    cY = 2022;
+
+                    int yourAge = cY - bY;
+
+                    Console.WriteLine("What year were you born");
+                    Console.WriteLine("I was born in 1991, so I am " + yourAge); ;
+                    Console.ReadLine();
+
+                    
+
+
         }
     }
 }
